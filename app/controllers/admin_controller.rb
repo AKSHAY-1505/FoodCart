1da3290c7843e2f02990fd330f19a4df53c83286
Akshay Balamurugan <akbalamurugan@C02F71ENMD6M.local>
2024-04-22 15:46:50 +0530

    add authorization to admin pages

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,12 @@
 class AdminController < ApplicationController
+    before_action :authenticate_user
+
     def home
+    end
+
+    private
+
+    def authenticate_user
+        redirect_to root_path, alert: "You are not authorised to access this page!" unless current_user&.admin?
     end
 end
