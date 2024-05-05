1a01ec3177714f90db21bbb88ef0a46af730ceaa
Akshay Balamurugan <akshaybalan31@gmail.com>
2024-05-06 04:17:20 +0530

    fix ajax for coupon

--- app/javascript/coupon.js
+++ app/javascript/coupon.js
@@ -1,30 +1,9 @@
 $(document).ready(function () {
-  $("#coupon-create-form").on("submit", function (e) {
-    e.preventDefault();
-    form = $(this);
-    url = form.attr("action");
-    data = new FormData(form[0]);
-
-    // Option Create AJAX
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: data,
-      processData: false,
-      contentType: false,
-      success: function (response) {
-        $(".coupons").append($(response));
-      },
-      error: function (error) {
-        console.error("Error:", error);
-      },
-    });
-  });
-
-  $(".coupon-delete-button").on("click", function (e) {
+  $(".container").on("click", function (e) {
     element = $(e.target);
+    console.log(element);
     if (element.hasClass("coupon-delete-button")) {
-      id = $(this).data("coupon-id");
+      id = $(element).data("coupon-id");
       form = $(this).parent();
       url = "/coupons/" + id;
       console.log(url);
@@ -33,6 +12,10 @@ $(document).ready(function () {
       $.ajax({
         url: url,
         type: "DELETE",
+        headers: {
+          // Alternatively you can skip authenticity token verification in the controller
+          "X-CSRF-Token": $('meta[name="csrf-token"]').attr("content"),
+        },
         success: function (response) {
           $(`#coupon_${id}`).remove();
         },
@@ -42,4 +25,28 @@ $(document).ready(function () {
       });
     }
   });
+
+  $("#coupon-create-form")
+    .off("submit")
+    .on("submit", function (e) {
+      e.preventDefault();
+      form = $(this);
+      url = form.attr("action");
+      data = new FormData(form[0]);
+
+      // Option Create AJAX
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: data,
+        processData: false,
+        contentType: false,
+        success: function (response) {
+          $(".coupons").append($(response));
+        },
+        error: function (error) {
+          console.error("Error:", error);
+        },
+      });
+    });
 });
